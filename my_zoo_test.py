feb4f060808281506affb71ced1f0aa5df284f76
Kamivision <kamivision@gmail.com>
2026-02-11 15:11:21 -0600

    finished bird class

--- my_zoo_test.py
+++ my_zoo_test.py
@@ -1,6 +1,22 @@
-from zoo_manager import Animal
+from zoo_manager import Animal, Mammal, Bird
 # Create an instance of the Animal class
 simba = Animal('Simba', 'Lion')
 
 print(simba)
 print(simba.speak('Roar'))
+
+# Create an instance of the Mammal class
+nala = Mammal('Nala', 'Lion')
+
+print(nala)
+print(nala.give_birth())
+
+# Create an instance of the Bird class
+# zazu = Bird('Zazu', 'bird', 12)
+
+# print(zazu)
+
+def test_bird():
+    bird = Bird("Eagle", "Aquila chrysaetos", wingspan=2.5)
+    assert bird.wingspan == 2.5
+    
--- zoo_manager.py
+++ zoo_manager.py
@@ -33,3 +33,30 @@ class Animal():
             self._species = species_val.strip().title()
         else:
             raise Exception("The species should be a `str` value")
+
+        
+class Mammal(Animal):
+    def __init__(self, name, species):
+        super().__init__(name, species)
+        
+    def give_birth(self):
+        return f"{self.name} the {self.species} has given birth"
+        
+class Bird(Animal):
+    def __init__(self, name, species, wingspan):
+        super().__init__(name, species)
+        self.wingspan = wingspan
+    
+    def __str__(self):
+        return f"{self.name} is a {self.species} with a {self.wingspan} wingspan"
+        
+    @property
+    def wingspan(self):
+        return self._wingspan
+    
+    @wingspan.setter
+    def wingspan(self, num):
+        if isinstance(num, int) and num > 0:
+            self._wingspan = num
+        else:
+            raise Exception("This must be a Positive Int > 0")
